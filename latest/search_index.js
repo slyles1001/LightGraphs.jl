7904717c53524592d320f56805e63158091045ee
autodocs <autodocs>
2018-01-14 20:20:26 +0000

    build based on f93fbf8

--- latest/search_index.js
+++ latest/search_index.js
@@ -1693,7 +1693,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Linear Algebra",
     "title": "LightGraphs.LinAlg.symmetrize",
     "category": "Function",
-    "text": "symmetrize(adjmat, which=:or)\n\nReturn a symmetric version of graph (represented by CombinatorialAdjacency adjmat) as a CombinatorialAdjacency. which may be one of :triu, :tril, :sum, or :or. Use :sum for weighted graphs.\n\nImplementation Notes\n\nOnly works on Adjacency because the normalizations don't commute with symmetrization.\n\n\n\n"
+    "text": "symmetrize(A::SparseMatrix, which=:or)\n\nReturn a symmetric version of graph (represented by sparse matrix A) as a sparse matrix. which may be one of :triu, :tril, :sum, or :or. Use :sum for weighted graphs.\n\n\n\n"
 },
 
 {
@@ -1701,7 +1701,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Linear Algebra",
     "title": "LightGraphs.LinAlg.symmetrize",
     "category": "Function",
-    "text": "symmetrize(A::SparseMatrix, which=:or)\n\nReturn a symmetric version of graph (represented by sparse matrix A) as a sparse matrix. which may be one of :triu, :tril, :sum, or :or. Use :sum for weighted graphs.\n\n\n\n"
+    "text": "symmetrize(adjmat, which=:or)\n\nReturn a symmetric version of graph (represented by CombinatorialAdjacency adjmat) as a CombinatorialAdjacency. which may be one of :triu, :tril, :sum, or :or. Use :sum for weighted graphs.\n\nImplementation Notes\n\nOnly works on Adjacency because the normalizations don't commute with symmetrization.\n\n\n\n"
 },
 
 {
@@ -1926,94 +1926,6 @@ var documenterSearchIndex = {"docs": [
     "title": "Full Docs",
     "category": "section",
     "text": "Modules = [LightGraphs]\nPages   = [\"degeneracy.jl\"]\nPrivate = false"
-},
-
-{
-    "location": "flowcut.html#",
-    "page": "Flow and Cut",
-    "title": "Flow and Cut",
-    "category": "page",
-    "text": ""
-},
-
-{
-    "location": "flowcut.html#Flow-and-Cut-1",
-    "page": "Flow and Cut",
-    "title": "Flow and Cut",
-    "category": "section",
-    "text": "LightGraphs.jl provides different algorithms for maximum flow and minimum cut computations, including:Order = [:type, :function]\nPages   = [\"flowcut.md\"]"
-},
-
-{
-    "location": "flowcut.html#LightGraphs.BoykovKolmogorovAlgorithm",
-    "page": "Flow and Cut",
-    "title": "LightGraphs.BoykovKolmogorovAlgorithm",
-    "category": "Type",
-    "text": "BoykovKolmogorovAlgorithm <: AbstractFlowAlgorithm\n\nForces the maximum_flow function to use the Boykov-Kolmogorov algorithm.\n\n\n\n"
-},
-
-{
-    "location": "flowcut.html#LightGraphs.DinicAlgorithm",
-    "page": "Flow and Cut",
-    "title": "LightGraphs.DinicAlgorithm",
-    "category": "Type",
-    "text": "DinicAlgorithm <: AbstractFlowAlgorithm\n\nForces the maximum_flow function to use Dinic's algorithm.\n\n\n\n"
-},
-
-{
-    "location": "flowcut.html#LightGraphs.EdmondsKarpAlgorithm",
-    "page": "Flow and Cut",
-    "title": "LightGraphs.EdmondsKarpAlgorithm",
-    "category": "Type",
-    "text": "EdmondsKarpAlgorithm <: AbstractFlowAlgorithm\n\nForces the maximum_flow function to use the Edmonds–Karp algorithm.\n\n\n\n"
-},
-
-{
-    "location": "flowcut.html#LightGraphs.PushRelabelAlgorithm",
-    "page": "Flow and Cut",
-    "title": "LightGraphs.PushRelabelAlgorithm",
-    "category": "Type",
-    "text": "Forces the maximum_flow function to use the Push-Relabel algorithm.\n\n\n\n"
-},
-
-{
-    "location": "flowcut.html#LightGraphs.maximum_flow",
-    "page": "Flow and Cut",
-    "title": "LightGraphs.maximum_flow",
-    "category": "Function",
-    "text": "maximum_flow(flow_graph, source, target[, capacity_matrix][, algorithm][, restriction])\n\nGeneric maximum_flow function for flow_graph from source to target with capacities in capacity_matrix. Uses flow algorithm algorithm and cutoff restriction restriction.\n\nIf capacity_matrix is not specified, DefaultCapacity(flow_graph) will be used.\nIf algorithm is not specified, it will default to PushRelabelAlgorithm.\nIf restriction is not specified, it will default to 0.\n\nReturn a tuple of (maximum flow, flow matrix). For the Boykov-Kolmogorov algorithm, the associated mincut is returned as a third output.\n\nUsage Example:\n\njulia> flow_graph = SimpleDiGraph(8) # Create a flow-graph\njulia> flow_edges = [\n(1,2,10),(1,3,5),(1,4,15),(2,3,4),(2,5,9),\n(2,6,15),(3,4,4),(3,6,8),(4,7,16),(5,6,15),\n(5,8,10),(6,7,15),(6,8,10),(7,3,6),(7,8,10)\n]\n\njulia> capacity_matrix = zeros(Int, 8, 8)  # Create a capacity matrix\n\njulia> for e in flow_edges\n    u, v, f = e\n    add_edge!(flow_graph, u, v)\n    capacity_matrix[u,v] = f\nend\n\njulia> f, F = maximum_flow(flow_graph, 1, 8) # Run default maximum_flow (push-relabel) without the capacity_matrix\n\njulia> f, F = maximum_flow(flow_graph, 1, 8, capacity_matrix) # Run default maximum_flow with the capacity_matrix\n\njulia> f, F = maximum_flow(flow_graph, 1, 8, capacity_matrix, algorithm=EdmondsKarpAlgorithm()) # Run Edmonds-Karp algorithm\n\njulia> f, F = maximum_flow(flow_graph, 1, 8, capacity_matrix, algorithm=DinicAlgorithm()) # Run Dinic's algorithm\n\njulia> f, F, labels = maximum_flow(flow_graph, 1, 8, capacity_matrix, algorithm=BoykovKolmogorovAlgorithm()) # Run Boykov-Kolmogorov algorithm\n\n\n\n\n"
-},
-
-{
-    "location": "flowcut.html#LightGraphs.ExtendedMultirouteFlowAlgorithm",
-    "page": "Flow and Cut",
-    "title": "LightGraphs.ExtendedMultirouteFlowAlgorithm",
-    "category": "Type",
-    "text": "ExtendedMultirouteFlowAlgorithm\n\nUsed to specify the Extended Multiroute Flow algorithm.\n\n\n\n"
-},
-
-{
-    "location": "flowcut.html#LightGraphs.KishimotoAlgorithm",
-    "page": "Flow and Cut",
-    "title": "LightGraphs.KishimotoAlgorithm",
-    "category": "Type",
-    "text": "KishimotoAlgorithm\n\nUsed to specify the Kishimoto algorithm.\n\n\n\n"
-},
-
-{
-    "location": "flowcut.html#LightGraphs.multiroute_flow-Union{Tuple{LightGraphs.AbstractGraph,Integer,Integer,AbstractArray{T,2}}, Tuple{LightGraphs.AbstractGraph,Integer,Integer}, Tuple{R}, Tuple{T}} where T where R<:Real",
-    "page": "Flow and Cut",
-    "title": "LightGraphs.multiroute_flow",
-    "category": "Method",
-    "text": "multiroute_flow(flow_graph, source, target[, DefaultCapacity][, flow_algorithm][, mrf_algorithm][, routes])\n\nThe generic multiroute_flow function.\n\nThe output will vary depending on the input:\n\nWhen the number of routes is 0, return the set of breaking points of\n\nthe multiroute flow.\n\nWhen the number of routes is 1, return a flow with a set of 1-disjoint paths\n\n(this is the classical max-flow implementation).\n\nWhen the input is limited to a set of breaking points and a route value k,\n\nreturn only the k-route flow.\n\nOtherwise, a tuple with 1) the maximum flow and 2) the flow matrix. When the\n\nmax-flow subroutine is the Boykov-Kolmogorov algorithm, the associated mincut is returned as a third output.\n\nWhen the input is a network, it requires the following arguments:\n\nflow_graph: the input graph\nsource: the source vertex\ntarget: the target vertex\ncapacity_matrix: matrix of edge flow capacities\nflow_algorithm: keyword argument for flow algorithm\nmrf_algorithm: keyword argument for multiroute flow algorithm\nroutes: keyword argument for the number of routes\n\nWhen the input is only the set of (breaking) points and the number of route, it requires the following arguments:\n\nbreakingpoints: vector of breaking points\nroutes: number of routes\n\nWhen the input is the set of (breaking) points, the number of routes, and the network descriptors, it requires the following arguments:\n\nbreakingpoints: vector of breaking points\nroutes: number of routes\nflow_graph: the input graph\nsource: the source vertex\ntarget: the target vertex\ncapacity_matrix: matrix of edge flow capacities\nflow_algorithm: keyword argument for flow algorithm\n\nThe function defaults to the Push-relabel (classical flow) and Kishimoto (multiroute) algorithms. Alternatively, the algorithms to be used can also be specified through  keyword arguments. A default capacity of 1 is assumed for each link if no capacity matrix is provided.\n\nThe mrf_algorithm keyword is inforced to Extended Multiroute Flow in the following cases:\n\nThe number of routes is non-integer\nThe number of routes is 0 or non-specified\n\nUsage Example :\n\n(please consult the  maximum_flow section for options about flow_algorithm and capacity_matrix)\n\njulia> flow_graph = SimpleDiGraph(8) # Create a flow graph\n\njulia> flow_edges = [\n(1, 2, 10), (1, 3, 5),  (1, 4, 15), (2, 3, 4),  (2, 5, 9),\n(2, 6, 15), (3, 4, 4),  (3, 6, 8),  (4, 7, 16), (5, 6, 15),\n(5, 8, 10), (6, 7, 15), (6, 8, 10), (7, 3, 6),  (7, 8, 10)\n]\n\njulia> capacity_matrix = zeros(Int, 8, 8) # Create a capacity matrix\n\njulia> for e in flow_edges\n    u, v, f = e\n    add_edge!(flow_graph, u, v)\n    capacity_matrix[u, v] = f\nend\n\njulia> f, F = multiroute_flow(flow_graph, 1, 8, capacity_matrix, routes = 2) # Run default multiroute_flow with an integer number of routes = 2\n\njulia> f, F = multiroute_flow(flow_graph, 1, 8, capacity_matrix, routes = 1.5) # Run default multiroute_flow with a noninteger number of routes = 1.5\n\njulia> points = multiroute_flow(flow_graph, 1, 8, capacity_matrix) # Run default multiroute_flow for all the breaking points values\n\njulia> f, F = multiroute_flow(points, 1.5) # Then run multiroute flow algorithm for any positive number of routes\n\njulia> f = multiroute_flow(points, 1.5, valueonly = true)\n\njulia> f, F, labels = multiroute_flow(flow_graph, 1, 8, capacity_matrix, algorithm = BoykovKolmogorovAlgorithm(), routes = 2) # Run multiroute flow algorithm using Boykov-Kolmogorov algorithm as maximum_flow routine\n\n\n\n\n"
-},
-
-{
-    "location": "flowcut.html#Full-Docs-1",
-    "page": "Flow and Cut",
-    "title": "Full Docs",
-    "category": "section",
-    "text": "Modules = [LightGraphs]\nPages   = [ \"flow/boykov_kolmogorov.jl\",\n            \"flow/dinic.jl\",\n            \"flow/edmonds_karp.jl\",\n            \"flow/ext_multiroute_flow.jl\",\n            \"flow/kishimoto\",\n            \"flow/maximum_flow.jl\",\n            \"flow/multiroute_flow.jl\",\n            \"flow/push_relabel.jl\"\n            ]\nPrivate = false"
 },
 
 {
